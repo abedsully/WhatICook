64346dcbab6f3fe8b2fac715fc397a9de8cd690d
abedyusop0 <stefanus.wilson@binus.ac.id>
2024-03-30 20:19:30 +0700

    Initial Commit

---
+++ WhatICook/WhatICookApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatICookApp.swift
+//  WhatICook
+//
+//  Created by Stefanus Albert Wilson on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatICookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
